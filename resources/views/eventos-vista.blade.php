3d199e02e95f1413b8e0242c4a28931a3a6bc559
rodriAcha28 <201900441@est.umss.edu>
2023-10-12 22:03:35 -0400

    Ivan no hizo esto

--- resources/views/eventos-vista.blade.php
+++ resources/views/eventos-vista.blade.php
@@ -12,48 +12,7 @@
 
 </head>
 <body>
-    <!--<nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href="https://flowbite.com/" class="flex items-center">
-        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo">
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-        
-        
-        
     
-
-
-    </a>
-    <div class="flex md:order-2">
-        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
-        <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
-            <span class="sr-only">Open main menu</span>
-            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-            </svg>
-        </button>
-    </div>
-    <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-        <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-        <li>
-            <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
-        </li>
-        <li>
-            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
-        </li>
-        <li>
-            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
-        </li>
-        <li>
-            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
-        </li>
-        </ul>
-        <div class="card mb-3" style="max-width: 540px;">
-    </div>
-    </div>
-    </div>
-</nav>-->
-
 
     <div class="sidebar">
         <div class="top">
@@ -64,7 +23,7 @@
             <i class="bx bx-menu" id="btn"></i>
         </div>
         <div class="user">
-            <img src="" alt="me" class="user-img">
+            <img src="{{ asset('userrr.png') }}" alt="me" class="user-img">
             <div>
                 <p class="bold">Admin B.</p>
                 <p>Admin</p>
@@ -82,18 +41,18 @@
             <li>
                 <a href="#">
                     <i class="bx bx-plus-medical"></i>
-                    <span class="nav-item">Crear_Evento</span>
+                    <span class="nav-item">Crear</span>
 
                 </a>
-                <span class="tooltrip">Crear_Evento</span>
+                <span class="tooltrip">Crear</span>
             </li>
             <li>
                 <a href="#">
                     <i class="bx bx-edit-alt"></i>
-                    <span class="nav-item">Editar-Event</span>
+                    <span class="nav-item">Editar</span>
 
                 </a>
-                <span class="tooltrip">Editar-Event</span>
+                <span class="tooltrip">Editar</span>
             </li>
             
         </ul>
@@ -102,19 +61,46 @@
 
 
     <div class="main-content">
-        <div class="conteiner">
+        <!--<div class="conteiner">
             <h1>Code Comerce</h1>
+        </div>-->
+        <div class="navbar">
+            <div class="titulo">Mi Sitio Web</div>
+            <div class="botones">
+                <button class="boton">Botón 1</button>
+                <button class="boton">Botón 2</button>
+                <button class="boton">Botón 3</button>
+            </div>
+        </div>
+
+        <div class="bodyEvents">
+            <!--<div class="card">
+                <div class="img-card"></div>
+                <div class="content">
+                    <div class="title">
+                        <h3>Evento 1</h3>
+                        <span>1 dia antes</span>
+                    </div>
+                    <div class="textEvent">
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis enim similique quasi, explicabo cumque dolore incidunt impedit molestias deserunt? Quos sapiente corporis ea rerum aliquid nulla praesentium veritatis distinctio laudantium.</p>
+                    </div>
+                    <div class="btn-container">
+                        <button>Ver mas</button>
+                    </div>
+                </div>
+            </div>-->
+            
+            <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700" class="cards">
+                <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/docs/images/blog/image-4.jpg" alt="">
+                <div class="flex flex-col justify-between p-4 leading-normal">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                </div>
+            </a>
+
         </div>
     </div>
-    <div class="divEvents">
-        <a href="#" id="eventsviews" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-            <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/docs/images/blog/image-4.jpg" alt="">
-            <div class="flex flex-col justify-between p-4 leading-normal">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Evento 1</h5>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-            </div>
-        </a>
-    </div>
+
 </body>
 
 <script>
