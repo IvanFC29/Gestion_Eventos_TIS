96e3755ab1117043c63b8e3b5b3b437661565c3e
Ivan <201900444@est.umss.edu>
2023-11-10 01:06:18 -0400

    Arreglo del form de correo

--- resources/views/enviarCorreoCoach.blade.php
+++ resources/views/enviarCorreoCoach.blade.php
@@ -1,65 +1,98 @@
-@extends('layouts.landing')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
+  <link rel="stylesheet" href="{{ asset('css/loginAdmin.css') }}" type="text/css">   
+  <link rel="stylesheet" href="{{ asset('css/correo.css') }}" type="text/css">   
+  <link rel="stylesheet" href="{{ asset('css/eventos_admin.css') }}" TYPE="text/css">  
+  <link rel="stylesheet" href="{{ asset('css/fondoazulito.css') }}" TYPE="text/css"> 
+   <link rel="stylesheet" href="{{ asset('css/formulario.css') }}" TYPE="text/css"> 
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> 
+    
+    
+    <!-- Tailwind CSS Link -->
+    <link rel="stylesheet" 
+    href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.1/tailwind.min.css">
 
-@section('content')
-<style>
-    .container {
-        background-color: #fff;
-        max-width: 600px;
-        margin: 20px auto; /* Centra el contenedor horizontalmente en la página */
-        padding: 20px;
-        border-radius: 5px;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        text-align: left; /* Restablece la alineación del texto a la izquierda dentro del contenedor */
-    }
+</head>
+<body>
+  <div id="background-container">
+    <img src="{{ asset('images/fondo3.jpg') }}" alt="Fondo de la página">  
+  </div>
+      <!--MENU SIDEBAR-->  
+    <div id="wrapper">
+        <div class="overlay"></div>
+          
+          <!-- Sidebar -->
+            <nav class="navbar navbar-inverse fixed-top" id="sidebar-wrapper" role="navigation">
+                <ul class="nav sidebar-nav">
+                <div class="sidebar-header">
+                    <div class="sidebar-brand ">
+                    <i class="bi bi-person-circle"></i>
+                    <a href="#">Admin</a>
+                    </div>        
+                </div>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/">Inicio</a>
+                </li>
+                <li class="dropdown">
+                    <a href="#works" class="dropdown-toggle"  data-toggle="dropdown">Eventos <span class="caret"></span></a>
+                    <ul class="dropdown-menu animated fadeInLeft" role="menu">
+                    <li><a href="/crear-evento" class="dropdown-item ">Crear Evento</a></li>
+                    <li><a href="/ver-eventos-editables">Eventos Editables</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="#works" class="dropdown-toggle"  data-toggle="dropdown">Coachs <span class="caret"></span></a>
+                    <ul class="dropdown-menu animated fadeInLeft" role="menu">
+                    <li><a href="/registerCoach" class="dropdown-item ">Nuevo Coach</a></li>
+                    <li><a href="#">Ver coachs</a></li>
+                    </ul>
+                </li>
+                <li><a href="#about">Reportes</a></li>
+                <li><a href="#events">Calendario</a></li>
+                @if(auth()->check())
+                    <li>
+                      <a href="{{ route('login.destroy') }}" >Cerrar Sesion</a>
+                    </li>
+                    @else
+                    <li class="mx-6">
+                      <a href="{{ route('login.index') }}" class="font-semibold hover-bg-indigo-700 py-3 px-4 rounded-md">Iniciar Sesión</a>
+                    </li>
+                    @endif
+                
+                </ul>
+            </nav>
+                <!-- /#sidebar-wrapper -->
 
-    h2 {
-        color: #333;
-    }
+                <!-- Page Content -->
+                <nav class="navbar custom-navbar navbar-expand-lg">
+                  <div class="container-fluid">
+                    <div class="divtitulom">
+                      <h1 class="titulomalo">Competencias - Programación Bolivia</h1>
+                    </div>
+                  </div>
+                </nav>
+                
+              <div id="page-content-wrapper">
+                  <button type="button" class="hamburger animated fadeInLeft is-closed" data-toggle="offcanvas">
+                      <span class="hamb-top"></span>
+                <span class="hamb-middle"></span>
+              <span class="hamb-bottom"></span>
+                  </button>
+                  
+              </div>
+              <!-- /#page-content-wrapper -->
+        </div>
+    </div>
 
-    label {
-        font-weight: bold;
-        display: block;
-        margin-top: 10px;
-    }
-
-    input[type="email"] {
-        width: 100%;
-        padding: 10px;
-        margin-top: 5px;
-        border: 1px solid #ccc;
-        border-radius: 5px;
-    }
-
-    .button-container {
-        text-align: center; /* Centra los botones horizontalmente */
-    }
-
-    .button {
-        background-color: #007BFF;
-        color: #fff;
-        padding: 10px 20px;
-        text-decoration: none;
-        border-radius: 5px;
-        
-        display: inline-block;
-        width: 100%;
-        height: 80%;
-    }
-
-    .button:hover {
-        background-color: #0056b3;
-    }
-    .large-heading {
-        font-weight: bold;
-        display: block;
-    text-align: center;
-    font-size: 24px;
-    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.0);
-}
-</style>
-
-
-    <h2 class="large-heading">Envio de Cuentas</h2>
+<div id="formCorreo">
+	<h2 class="large-heading">Envio de Cuentas</h2>
     <br>
     <form action="/enviar-cuenta-coach" method="POST">
         @csrf
@@ -84,9 +117,44 @@
     @endif
 
     </form>
+</div>
     
     
+      <script>
+            $(document).ready(function () {
+              var trigger = $('.hamburger'),
+                  overlay = $('.overlay'),
+                isClosed = false;
 
+                trigger.click(function () {
+                  hamburger_cross();      
+                });
 
+                function hamburger_cross() {
 
-@endsection
+                  if (isClosed == true) {          
+                    overlay.hide();
+                    trigger.removeClass('is-open');
+                    trigger.addClass('is-closed');
+                    isClosed = false;
+                  } else {   
+                    overlay.show();
+                    trigger.removeClass('is-closed');
+                    trigger.addClass('is-open');
+                    isClosed = true;
+                  }
+              }
+              
+              $('[data-toggle="offcanvas"]').click(function () {
+                    $('#wrapper').toggleClass('toggled');
+              });  
+            });
+              $(document).ready(function() {
+                  // Cierra el modal al presionar el botón "Cerrar" dentro del modal
+                  $('.modal .close').on('click', function() {
+                      $(this).closest('.modal').modal('hide');
+                  });
+              });
+          </script>
+      </body>
+</html>
